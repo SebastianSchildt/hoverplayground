94972abc006ee461cba9919cff9e50171dd0ecf1
PN:Ruichao Wu2 <ruichao.wu@ipa.fraunhofer.de>
2021-12-29 15:28:45 +0100

    covert Arduino Reader data to speed and steer

--- read_port.py
+++ read_port.py
@@ -1,10 +1,10 @@
+#!/env/python3
+
 import serial
 import threading
 import json
 import queue
-
-#define MAX_LINEAR_VELOCITY              (WHEEL_RADIUS * 2 * 3.14159265359 * 61 / 60) // m/s  (BURGER : 61[rpm], WAFFLE : 77[rpm])
-#define MAX_ANGULAR_VELOCITY             (MAX_LINEAR_VELOCITY / TURNING_RADIUS)       // rad/s
+import argparse
 
 data_set = {"a0": -1 , "a1": -1}
 
@@ -35,14 +35,32 @@ class SerialReader(threading.Thread):
 
     def emitNewData(self,rxdata, range): # set scale
         a0, a1 = self.get(rxdata, range)
-        self.pipe.put( (int(a0), int(a1)) )
+        speed, steer = self.convert_to_speed_steer(a0, a1)
+        self.pipe.put( (int(speed), int(steer)) )
 
     def get(self, raw_data, range, range_raw = 1000):
+        get_data = {}
+        get_data["a0"] = 0
+        get_data["a1"] = 0
         with self.dataMutex:  # lock the buffer and copy the requested data out
-            get_data = json.loads(raw_data)
+            try:
+                get_data = json.loads(raw_data)
+            except json.decoder.JSONDecodeError:
+                pass
         a0 = get_data["a0"] *range / range_raw
         a1 = get_data["a1"] *range / range_raw
         return (a0, a1)
+
+    def convert_to_speed_steer(self, a0, a1):
+        "+ right"
+        "a1 right"
+        if a1 > a0:
+            speed = a1
+        else:
+            speed = a0
+        steer = a1 -a0
+        return (speed, steer)
+
 
     def exit(self):
         """ Instruct the serial thread to exit."""
@@ -50,10 +68,13 @@ class SerialReader(threading.Thread):
             self.exitFlag = True
 
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Give arduino port')
+    parser.add_argument('--port', metavar='N', type=str, default='/dev/ttyUSB0',
+                        help='arduino port')
+    args = parser.parse_args()
 
-    # Create thread to read and buffer serial data.
     pipe=queue.Queue()
-    thread = SerialReader("/dev/ttyACM1", pipe)
+    thread = SerialReader(args.port, pipe)
     thread.start()
 
     while True:
