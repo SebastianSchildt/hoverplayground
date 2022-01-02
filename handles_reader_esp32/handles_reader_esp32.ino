cdaf34b30e240242d6e39e17b7b637243d356633
Sebastian <sebastian@frozenlight.de>
2022-01-02 14:49:10 +0100

    Time based sending

--- handles_reader_esp32/handles_reader_esp32.ino
+++ handles_reader_esp32/handles_reader_esp32.ino
@@ -1,6 +1,11 @@
 #include <ArduinoJson.h>
 
-#define FILTER_LEN  100
+#define FILTER_LEN  64
+
+//Will only send new data if at least TIMEDELTA ms have elapses
+#define TIMEDELTA 100
+//timestamp of last transmissions (will roll over rafter 50 days, so no speial handling)
+unsigned long last_send_ts;
 
 const int A0Pin=34;
 const int A1Pin=35;
@@ -20,7 +25,8 @@ uint32_t A1_Filtered = 0;
 uint32_t A1_Max = 0;
 uint32_t A1_Min = 0xFFFFFFFF;
 
-DynamicJsonDocument doc(128);
+uint32_t samples_since_last_transmission=0;
+DynamicJsonDocument doc(255);
 
 uint32_t rescale(uint32_t input, uint32_t min_val, uint32_t max_val, uint32_t range, uint32_t min_thread) {
   float a = 0;
@@ -75,46 +81,57 @@ uint32_t readADC_Avg(uint32_t ADC_Raw, uint32_t *An_Buffer, uint16_t *An_i)
 }
 
 void setup() {
-  doc["a0"] = 0;
-  doc["a1"] = 0;
 
-  
   for(uint16_t i=0; i<FILTER_LEN; i++)
   {
     A0_Buffer[i] = analogRead(A0Pin);
     A1_Buffer[i] = analogRead(A1Pin);
   }
-  
+
+ last_send_ts=millis();
   Serial.begin(115200);
-  while (!Serial);
-  serializeJsonPretty(doc, Serial);
+  while (!Serial); //wait for serial
+  
 }
 
-void loop() {
 
+void send() {
+  doc["a0"]=rescale(A0_Filtered, 940, 3144, 1000, 10);
+  doc["a1"]=rescale(A1_Filtered, 942, 3138, 1000, 10);
+  doc["a0_min"] = A0_Min;
+  doc["a0_max"] = A0_Max;
+  doc["a1_min"] = A1_Min;
+  doc["a1_max"] = A1_Max;
+  doc["samples"] = samples_since_last_transmission;
+
+  char out[255];
+  serializeJson(doc, out);
+  strcat(out, "\n");
+  Serial.write(out);
+
+}
+
+
+void sampleInputs() {
+  samples_since_last_transmission++;
   A0_Raw = analogRead(A0Pin);
   A0_Filtered = readADC_Avg(A0_Raw, A0_Buffer, &A0_i);
   
   A1_Raw = analogRead(A1Pin);
   A1_Filtered = readADC_Avg(A1_Raw, A1_Buffer, &A1_i);
-
-  doc["a0"]=rescale(A0_Filtered, 940, 3144, 1000, 10);
-  doc["a1"]=rescale(A1_Filtered, 942, 3138, 1000, 10);
-  
   /** find min and max   **/
   find_min(A0_Filtered, &A0_Min);
   find_max(A0_Filtered, &A0_Max);
-  doc["a0_min"] = A0_Min;
-  doc["a0_max"] = A0_Max;
   
   find_min(A1_Filtered, &A1_Min);
-  find_max(A1_Filtered, &A1_Max);
-  doc["a1_min"] = A1_Min;
-  doc["a1_max"] = A1_Max;
+  find_max(A1_Filtered, &A1_Max);  
+}
 
-
-  char out[128];
-  serializeJson(doc, out);
-  strcat(out, "\n");
-  Serial.write(out);
+void loop() {
+  sampleInputs();
+  if (millis() - last_send_ts > TIMEDELTA) {
+    send();
+    last_send_ts=millis();
+    samples_since_last_transmission=0;
+  }
 }
