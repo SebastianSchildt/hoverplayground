91511061ed51bb1ba2bfc2c18ddd8dbf9781abc7
ipa-rwu <ruichao.wu@ipa.fraunhofer.de>
2022-01-08 11:53:20 +0100

    send test byte data

--- testmsg.py
+++ testmsg.py
@@ -8,9 +8,9 @@ b=rxFrame.pack(0xABCD, 1,2,3,4,5,6,7,8)
 
 print(f"Will sent {len(b)} bytes")
 
-f=open("/dev/pts/5",'wb')
+f=open("/dev/ttyUSB0",'wb')
 
 f.write(b)
 
-f.write(b'0xff')
+f.write(b'\xcd\xab\x99\xffg\x00\xb8\xff\n\x00\xcf\x0e\xa4\x01\x00\x00\xea\xa4')
 f.close()
